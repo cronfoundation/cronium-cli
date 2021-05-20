4b3b2e2cb442ace242c9f4bccde15deb7f48314b
Alex Makarov <alex@cron-foundation.org>
2021-05-20 17:05:56 +0800

    add logging, update plugins packages

---
+++ Cron.CLI/Logging/CronLogger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Cron.Interface;
+using Serilog;
+
+namespace Cron.CLI.Logging
+{
+    public class CronLogger: ICronLogger
+    {
+        private readonly ILogger _logger;
+        public CronLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger.Information(message);
+        }
+
+        public void Warning(string message)
+        {
+            _logger.Warning(message);
+        }
+
+        public void Trace(string message)
+        {
+            _logger.Verbose(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+        
+        public void Error(Exception e)
+        {
+            _logger.Error(e, "Error occurred");
+        }
+
+        public void Error(Exception e, string error)
+        {
+            _logger.Error(e, error);
+        }
+    }
+}
